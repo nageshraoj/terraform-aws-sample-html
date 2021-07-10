39c9cac6cb5e5a79af10b8e4ed3024171750ec0c
Nageswara Rao <nageshraoj@gmail.com>
2021-07-10 19:50:22 +0530

    changed ami

--- instance.tf
+++ instance.tf
@@ -1,17 +1,17 @@
-# data "aws_ami" "myec2" {
-#   most_recent = true
-#   owners = [ "amazon" ]
-#   filter {
-#     name = "root-device-type"
-#     values = [ "ebs" ]
-#   }
+data "aws_ami" "myec2" {
+  most_recent = true
+  owners = [ "amazon" ]
+  filter {
+    name = "root-device-type"
+    values = [ "ebs" ]
+  }
 
-#   filter {
-#     name = "virtualization-type"
-#     values = [ "hvm" ]
-#   }
-#   filter {
-#      name = "name"
-#      values = [ "amzn2-ami-hvm-*" ]
-#   }
-# }
+  filter {
+    name = "virtualization-type"
+    values = [ "hvm" ]
+  }
+  filter {
+     name = "name"
+     values = [ "amzn2-ami-hvm-*" ]
+  }
+}
